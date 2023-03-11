a4a9f0391d323bf825c58998d3f914784037eab7
Joao Victor Botelho <joaono2@hotmail.com>
2023-03-11 18:00:33 +0000

    wip

--- glitched-cat-front-angular/src/app/app.component.ts
+++ glitched-cat-front-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'glitched-cat-front-angular';
+  title = 'Glitched Cat Blog';
 }
